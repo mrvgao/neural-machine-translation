0e77c21252de9fb35a8bfe98fd347a2670e36b73
fortymiles <mqgao@outlook.com>
2017-09-13 16:22:27 +0800

    dataset iterator is complete.

--- data_utils/iterator_utils.py
+++ data_utils/iterator_utils.py
@@ -2,43 +2,61 @@ import tensorflow as tf
 from tensorflow.contrib import data as tf_data
 from data_utils import table_utils
 
-source_eos = '.'
-target_eos = '-'
+unk = '*'
+sos = '.'
+eos = '-'
+eos_id = 1
 
-source_eos_id = 2
-target_eos_id = 2
 
-source_dataset = tf_data.TextLineDataset('source.txt')
-target_dataset = tf_data.TextLineDataset('target.txt')
+def get_iterator(src_file, tgt_file, src_vocab_file, tgt_vocab_file):
+    source_dataset = tf_data.TextLineDataset(src_file)
+    target_dataset = tf_data.TextLineDataset(tgt_file)
 
-src_file, src_size = table_utils.check_vocab('source.txt', eos=source_eos)
-tgt_file, tgt_size = table_utils.check_vocab('target.txt', eos=target_eos)
+    src_file, src_size = table_utils.check_vocab(src_vocab_file, eos=eos, unk=unk, sos=sos)
+    tgt_file, tgt_size = table_utils.check_vocab(tgt_vocab_file, eos=eos, unk=unk, sos=sos)
 
-src_table, tgt_table = table_utils.create_vocab_tables(src_vocab_file=src_file, tgt_vocab_file=tgt_file)
+    src_table, tgt_table = table_utils.create_vocab_tables(src_vocab_file=src_file, tgt_vocab_file=tgt_file)
 
-source_dataset = source_dataset.map(lambda string: tf.string_split([string]).values)
-source_dataset = source_dataset.map(lambda words: (words, tf.size(words)))
-source_dataset = source_dataset.map(lambda words, size: (src_table.lookup(words), size))
+    source_dataset = source_dataset.map(lambda string: tf.string_split([string]).values)
+    source_dataset = source_dataset.map(lambda words: (words, tf.size(words)))
+    source_dataset = source_dataset.map(lambda words, size: (src_table.lookup(words), size))
 
-target_dataset = target_dataset.map(lambda string: tf.string_split([string]).values)
-target_dataset = target_dataset.map(lambda words: (words, tf.size(words)))
-target_dataset = target_dataset.map(lambda words, size: (tgt_table.lookup(words), size))
+    target_dataset = target_dataset.map(lambda string: tf.string_split([string]).values)
+    target_dataset = target_dataset.map(lambda words: (words, tf.size(words)))
+    target_dataset = target_dataset.map(lambda words, size: (tgt_table.lookup(words), size))
 
-source_target_dataset = tf_data.Dataset.zip((source_dataset, target_dataset))
+    source_target_dataset = tf_data.Dataset.zip((source_dataset, target_dataset))
 
-batch_size = 256
+    # TODO<minquan>: Add similar size batched.
 
-batched_dataset = source_target_dataset.padded_batch(
-    batch_size,
-    padded_shapes=((tf.TensorShape([None]), tf.TensorShape([])),
-                   (tf.TensorShape([None]), tf.TensorShape([]))),
-    padding_values=((tf.cast(source_eos_id, tf.int64), 0),
-                    (tf.cast(target_eos_id, tf.int64), 0))
-)
+    batch_size = 256
 
-batched_iterator = batched_dataset.make_initializable_iterator()
+    batched_dataset = source_target_dataset.padded_batch(
+        batch_size,
+        padded_shapes=((tf.TensorShape([None]), tf.TensorShape([])),
+                       (tf.TensorShape([None]), tf.TensorShape([]))),
+        padding_values=((tf.cast(eos_id, tf.int64), 0),
+                        (tf.cast(eos_id, tf.int64), 0))
+    )
 
-((source, source_length), (target, target_length)) = batched_iterator.get_next()
+    batched_iterator = batched_dataset.make_initializable_iterator()
 
-with tf.Session() as sess:
-    sess.run(batched_iterator.initializer)
+    ((source, source_length), (target, target_length)) = batched_iterator.get_next()
+
+    return batched_iterator, source, source_length, target, target_length
+
+
+if __name__ == '__main__':
+
+    batched_iterator, source, source_length, target, target_length = get_iterator(
+        src_file='source.txt', src_vocab_file='source_vocab.txt',
+        tgt_file='target.txt', tgt_vocab_file='target_vocab.txt'
+    )
+
+    with tf.Session() as sess:
+        tf.tables_initializer().run()
+        batched_iterator.initializer.run()
+        src, src_length = sess.run([source, source_length])
+
+        print(src[:10])
+        print(src_length[:10])
