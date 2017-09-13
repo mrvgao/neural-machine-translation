777700385efbc3254c215edd631935928e58161d
fortymiles <mqgao@outlook.com>
2017-09-13 21:31:30 +0800

    attention is okay.

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 
 import tensorflow as tf
@@ -6,6 +7,8 @@ from tensorflow.contrib import seq2seq
 from tensorflow.python.layers import core as layers_core
 
 from data_utils import iterator_utils
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '4, 5, 6'
 
 src_vocab_size = 29
 tgt_vocab_size = 15
@@ -18,7 +21,8 @@ dtype = tf.float32
 time_major = True
 
 Hyperpamamters = namedtuple('hps', ['learning_rate', 'batch_size',
-                                    'max_gradient_norm', 'num_units'])
+                                    'max_gradient_norm', 'num_units',
+                                    'attention'])
 
 
 class Model:
@@ -26,7 +30,6 @@ class Model:
         self.iterator = iterator
         self.time_major = True
         self.hps = hps
-
         self.__build_embedding__()
 
     def __build_embedding__(self):
@@ -56,7 +59,25 @@ class Model:
             )
 
     def decode(self):
+        attention_states = tf.transpose(self.encoder_outputs, [1, 0, 2])
+
+        attention_mechanism = seq2seq.LuongAttention(
+            num_units=self.hps.num_units, memory=attention_states,
+            memory_sequence_length=self.iterator.source_length
+        )
+
         decoder_cell = rnn.BasicLSTMCell(num_units=self.hps.num_units)
+
+        decoder_cell = seq2seq.AttentionWrapper(
+            decoder_cell, attention_mechanism,
+            attention_layer_size=self.hps.num_units,
+            name='attention'
+        )
+
+        decoder_initial_state = decoder_cell.zero_state(batch_size=self.hps.batch_size, dtype=dtype).clone(
+            cell_state=self.encoder_state
+        )
+
         helper = seq2seq.TrainingHelper(
             self.decoder_emb_inp, iterator.target_length, time_major=True
         )
@@ -64,7 +85,7 @@ class Model:
         projection_layer = layers_core.Dense(tgt_vocab_size, use_bias=False)
 
         decoder = seq2seq.BasicDecoder(
-            decoder_cell, helper, self.encoder_state, output_layer=projection_layer
+            decoder_cell, helper, decoder_initial_state, output_layer=projection_layer
         )
 
         outputs, _, _ = seq2seq.dynamic_decode(decoder)
@@ -119,7 +140,8 @@ if __name__ == '__main__':
         learning_rate=1e-3,
         batch_size=128,
         max_gradient_norm=5,
-        num_units=100
+        num_units=256,
+        attention=True
     )
 
     params = {
