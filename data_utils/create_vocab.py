865736c85576c4e10bf4eb38b0459a273d88c807
i-gaominquan <minquan@minquans-MacBook-Pro.local>
2017-09-19 13:10:31 +0800

    change dimension

--- data_utils/create_vocab.py
+++ data_utils/create_vocab.py
@@ -1,9 +1,9 @@
 source_vocab = open('source_vocab.txt', 'w')
 start = ord('a')
-words = '\n'.join([chr(i) for i in range(start, start + 26)])
+words = '\n'.join([chr(i) for i in range(start, start + 5)])
 source_vocab.write(words)
 
 target_vocab = open('target_vocab.txt', 'w')
 start = ord('0')
-words = '\n'.join([chr(i) for i in range(start, start + 10)])
+words = '\n'.join([chr(i) for i in range(start, start + 5)])
 target_vocab.write(words)
