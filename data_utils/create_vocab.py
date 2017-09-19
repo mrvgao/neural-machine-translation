de84d1b9115b26f3c1a6caa37365934bcd8fb9e2
fortymiles <mqgao@outlook.com>
2017-09-19 13:17:52 +0800

    change task to be simpler. a, b, c ==> A, B, C

--- data_utils/create_vocab.py
+++ data_utils/create_vocab.py
@@ -4,6 +4,6 @@ words = '\n'.join([chr(i) for i in range(start, start + 5)])
 source_vocab.write(words)
 
 target_vocab = open('target_vocab.txt', 'w')
-start = ord('0')
+start = ord('A')
 words = '\n'.join([chr(i) for i in range(start, start + 5)])
 target_vocab.write(words)
