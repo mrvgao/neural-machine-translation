2e9a29630abf9ef4e9cf3c3e16a55ffa876fafea
i-gaominquan <minquan@minquans-MacBook-Pro.local>
2017-09-19 13:26:36 +0800

    change task only two char. a and b.

--- data_utils/create_vocab.py
+++ data_utils/create_vocab.py
@@ -1,9 +1,9 @@
 source_vocab = open('source_vocab.txt', 'w')
 start = ord('a')
-words = '\n'.join([chr(i) for i in range(start, start + 5)])
+words = '\n'.join([chr(i) for i in range(start, start + 2)])
 source_vocab.write(words)
 
 target_vocab = open('target_vocab.txt', 'w')
 start = ord('A')
-words = '\n'.join([chr(i) for i in range(start, start + 5)])
+words = '\n'.join([chr(i) for i in range(start, start + 2)])
 target_vocab.write(words)
